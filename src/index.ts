7abb7e1661d4efa9e9596651c22e3db3c9ac2136
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-02-06 09:58:11 +0000

    Release 0.1.12

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * as AirweaveSDK from "./api";
 export { AirweaveSDKClient } from "./Client";
+export { AirweaveSDKEnvironment } from "./environments";
 export { AirweaveSDKError, AirweaveSDKTimeoutError } from "./errors";
