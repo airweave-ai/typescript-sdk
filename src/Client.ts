fe3645d85e6111743bd3dae08908768b78c6f368
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-12 14:44:32 +0000

    Release v0.5.12

--- src/Client.ts
+++ src/Client.ts
@@ -48,8 +48,8 @@ export class AirweaveSDKClient {
                 {
                     "X-Fern-Language": "JavaScript",
                     "X-Fern-SDK-Name": "@airweave/sdk",
-                    "X-Fern-SDK-Version": "v0.5.11",
-                    "User-Agent": "@airweave/sdk/v0.5.11",
+                    "X-Fern-SDK-Version": "v0.5.12",
+                    "User-Agent": "@airweave/sdk/v0.5.12",
                     "X-Fern-Runtime": core.RUNTIME.type,
                     "X-Fern-Runtime-Version": core.RUNTIME.version,
                 },
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "v0.5.11";
+export const SDK_VERSION = "v0.5.12";
